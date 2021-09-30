467ed1141a5eb0dc49a33bcf77ad8990031e7549
Tim Curtis <tcurtis@Tims-iMac.local>
2021-09-30 14:27:59 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+/* eslint-disable vue/require-v-for-key */
+<template>
+  <div id="app">
+    <div class="container mx-auto">
+      <div class="text-center my-10">
+        <h1 class="text-4xl">Guest List</h1>
+      </div>
+
+      <div class="max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <div class="flex items-center justify-between">
+            <span class="text-sm font-light text-gray-800 dark:text-gray-400" v-bind:class="formSubmitClass">{{ event.eventDate }}</span>
+        </div>
+
+        <div class="mt-2">
+            <h4 class="text-2xl font-bold text-gray-700 dark:text-white">{{ event.eventTitle }}</h4>
+            <p class="mt-2 text-gray-600 dark:text-gray-300">{{ event.eventDescription }}</p>
+            <p class="mt-2 text-gray-600 dark:text-gray-300">Add your name to the guest list for exclusive offers:</p>
+
+            <div class="flex items-center justify-center pb-6 md:py-0 md:w-1/2 mt-2">
+              <form v-on:submit.prevent="formSubmitted">
+                <div class="flex flex-col overflow-hidden border rounded-lg dark:border-gray-600 lg:flex-row">
+                  <input class="px-6 py-3 text-gray-700 placeholder-gray-500 bg-white outline-none dark:bg-gray-800 dark:placeholder-gray-400 focus:placeholder-transparent dark:focus:placeholder-transparent" type="text" placeholder="Jane Doe" v-model="newNameText">
+                  
+                  <button class="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 transition-colors duration-200 transform bg-gray-700 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none" >Submit</button>
+                </div>
+              </form>
+            </div>
+        </div>
+      </div>
+
+      <div class="mt-10 max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-lg dark:bg-gray-800">
+        <div class="flex items-center justify-between">
+            <span class="text-sm font-light text-gray-800 dark:text-gray-400">Attending ({{ guestName.length }})</span>
+        </div>
+
+        <div v-if="guestName.length > 0">
+          <!-- eslint-disable-next-line -->
+          <div class="mt-2 mr-2 inline-flex items-center justify-center text-base border-2 border-green-200 text-green-800 dark:text-white bg-green-400 rounded-md px-4 py-2" v-for="name in guestName">{{ name }}</div>
+        </div>
+
+        <div class="mt-2" v-else>
+          <h4 class="text-2xl font-bold text-gray-700 dark:text-white">No names added yet...</h4>
+        </div>
+      </div>
+
+    </div>
+    <img class="mx-auto" alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: function(){
+    return {
+      event: {
+          eventDate: 'August 14th- 16th',
+          eventTitle: 'Summer Festival!',
+          eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+        },
+        newNameText: '',
+        guestName: [],
+        formSubmitClass: ""
+    }
+  },
+  methods: {
+    formSubmitted: function() {
+      if(this.newNameText.length > 0) {
+        this.guestName.push(this.newNameText)
+        this.newNameText = ''
+        this.formSubmitClass = "text-green-600 font-bold"
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/scss/app.scss"
+</style>
