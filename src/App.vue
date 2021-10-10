8ef4d279ac8502ee03d297a9d6aeaf7342342ead
Tim Curtis <tcurtis@Tims-iMac.local>
2021-10-10 11:06:14 -0400

    added progrss bar to atending section

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,12 @@
         <div class="flex items-center justify-between">
             <span class="text-sm font-light text-gray-800 dark:text-gray-400">Attending ({{ guestName.length }})</span>
         </div>
+        <br>
+        <div class="relative pt-1">
+          <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-green-200">
+            <div :style="{width: eventCapacityPercentage + '%'}" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
+          </div>
+        </div>
 
         <div v-if="guestName.length > 0">
           <!-- eslint-disable-next-line -->
@@ -61,10 +67,12 @@ export default {
           eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
         },
         newNameText: '',
-        guestName: ['Sarah', 'Lynn', 'Clayton'],
+        guestName: [],
         appStyles: {
           marginTop: '0.5rem'
-        }
+        },
+        eventCapacity: 25,
+        eventCapacityPercentage: 0
     }
   },
   methods: {
@@ -72,6 +80,7 @@ export default {
       if(this.newNameText.length > 0) {
         this.guestName.push(this.newNameText)
         this.newNameText = ''
+        this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100)
       }
     }
   }
